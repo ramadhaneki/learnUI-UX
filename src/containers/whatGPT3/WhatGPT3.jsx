6959d6850a3d0ce1fd6705f3ab55dcccefecc5b8
ramadhaneki <ramadhaneki91@gmail.com>
2023-02-18 18:33:00 +0700

    add brand

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import './whatgpt3.css'
 import React from 'react'
+import { Feature } from '../../components'
 
 function WhatGPT3() {
   return (
-    <div>
-      
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'> 
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond imaginations</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
     </div>
   )
 }
